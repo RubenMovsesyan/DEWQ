5baa8ff6eece8b35ad6090db6d5410ed4af0504c
Ruben Movsesyan <movsesyan.ruben@gmail.com>
2025-01-14 23:08:52 -0800

    fixed the cargo toml file

--- src/bit_utils/bit.rs
+++ src/bit_utils/bit.rs
@@ -23,7 +23,6 @@ macro_rules! impl_from_for_bit {
 
 impl_from_for_bit!(u8, u16, u32, u64, u128, usize, i8, i16, i32, i64, i128, isize);
 
-// #[cfg(any(test, feature = "test_feature"))]
 impl Display for Bit {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         match self {
